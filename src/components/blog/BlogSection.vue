26b7c1c46143e50abee22dc1b60635afea2af7f0
AnastasiiaPavlova94 <p.nastyaa1@gmail.com>
2022-07-25 23:49:20 +0300

    add gatablog

---
+++ src/components/blog/BlogSection.vue
@@ -0,0 +1,40 @@
+<template>
+<section>
+	<div class="container">
+	<div class="blog_section">
+		<h2>blog posts</h2>
+		<p>
+		Id at urna non viverra eu volutpat porttitor elementum. Viverra
+		senectus dui ultricies dolor.
+		</p>
+		<blog-card />
+	<router-link to="/all-blog" class="blg_btn">
+			<button class="btn">
+			View all blogs <span class="icon-long-arrow-right"></span>
+			</button>
+	</router-link>
+	</div>
+	</div>
+</section>
+</template>
+
+<script>
+import BlogCard from './BlogCard.vue';
+export default {
+ components: {
+	BlogCard
+}
+}
+</script>
+
+<style lang="scss">
+.blog_section{
+	display: flex;
+	flex-direction:column;
+	padding: 0 0 100px 0;
+	.blg_btn {
+		width: 200px;
+		margin: 0 auto;
+	}
+}
+</style>
---
+++ src/components/news/NewCard.vue
@@ -0,0 +1,104 @@
+<template>
+	<div class="container">
+		<router-link to="/all-blog" class="blg_btn">
+			<span class="come_back">← All blogs</span>
+		</router-link>
+		<h1>{{item.title}}</h1>
+			<div class="news">
+				<div class="news_content">
+					<h3 v-html="item.description"></h3>
+						<a :href="item.url" target="_blank" rel="nofollow, noreferrer">Full information</a>
+					<h5>Author: {{item.author}}</h5>
+				</div>
+				<img :src="item.urlToImage" alt="post1" />
+		</div>
+	</div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+	data(){
+		return{
+			item: {}
+		}
+	},
+	created(){
+		let url = `https://newsapi.org/v2/everything?q=${this.$route.params.title}&serchIn=title&sortBy=popularity&apiKey=4364238301bb4d329a06c6ae22a91354`
+		axios
+		.get(url)
+		.then(resp=>
+			this.item = resp.data.articles[0]
+		)
+	}
+}
+</script>
+
+<style lang="scss">
+
+	h1{
+	font-size: 3rem;
+	width: 800px;
+	text-align: center;
+	margin: 70px auto;
+	}
+	.news{
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		padding-bottom: 50px;
+		gap: 30px;
+		&_content{
+			margin: auto;
+			position: relative;
+		}
+		h5{
+			position: absolute;
+			right: 10px;
+			color: #818181;
+			}
+			a{
+			font-size: 1.7rem;
+			font-style: italic;
+			line-height: 200.9%;
+			-webkit-text-decoration-line: underline;
+			text-decoration-line: underline;
+			color: #91795C;
+			&:hover{
+				color: #424242;
+			}
+			}
+		img{
+			box-shadow: 0 0 40px #909090;
+		}
+	}
+	.come_back{
+		font-style: italic;
+		font-weight: 400;
+		font-size: 0.875rem;
+		line-height: 200.9%;
+		-webkit-text-decoration-line: underline;
+		text-decoration-line: underline;
+		color: #424242;
+		position: relative;
+		top: 40px;
+		&:hover{
+			color: #91795C;
+		}
+	}
+@media screen and (max-width: 850px) {
+	h1 {
+    font-size: 2rem;
+    width: auto;
+    text-align: center;
+	margin: 70px 0 30px 0;
+	}
+}
+@media screen and (max-width: 750px) {
+	.news {
+	grid-template-columns: 1fr;
+		a {
+		font-size: 1rem;
+		}
+	}
+}
+</style>
